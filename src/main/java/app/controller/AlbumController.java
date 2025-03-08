a44125554aa185d0899dc18b4a03fc479503611e
Dami-Lescano <lescano5600@gmail.com>
2025-03-08 19:39:48 -0300

    agregado get all album names

--- src/main/java/app/controller/AlbumController.java
+++ src/main/java/app/controller/AlbumController.java
@@ -1,5 +1,9 @@
 package app.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +19,11 @@ public class AlbumController extends GenericController<Album>{
 	protected AlbumController(AlbumService service) {
 		super(service);
 		this.albumService = service;
+	}
+	
+	@GetMapping("/names")
+	private ResponseEntity<List<String>> albumsNames() {
+		List<String> response = this.albumService.albumsNames();
+		return ResponseEntity.ok(response);
 	}
 }
--- src/main/java/app/service/AlbumService.java
+++ src/main/java/app/service/AlbumService.java
@@ -1,5 +1,7 @@
 package app.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import app.model.Album;
@@ -17,5 +19,11 @@ public class AlbumService extends GenericService<Album>{
 	public Album findById(Integer id) {
 		Album album = albumRepository.findByAlbumId(id);
 		return album;
+	}
+
+	public List<String> albumsNames() {
+		List<Album> albums = albumRepository.findAll();
+		List<String> names = albums.stream().map(a -> a.getTitle()).toList();
+		return names;
 	}
 }
