ca63fdaebfe848f4716c3435c4fb0e062281792c
Dami-Lescano <lescano5600@gmail.com>
2025-03-11 16:44:27 -0300

    Agregado country enum

---
+++ src/main/java/app/enumerate/Country.java
@@ -0,0 +1,40 @@
+package app.enumerate;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum Country {
+	UNKNOWN("Unknown", "Desconocido"),
+	CANADA("Canada", "Canadá"),
+	UNITED_STATES("United States", "Estados Unidos"),
+	UNITED_KINGDOM("United Kingdom", "Reino Unido"),
+	IRELAND("Ireland", "Irlanda"),
+	AUSTRALIA("Australia", "Australia"),
+	SWEDEN("Sweden", "Suecia");
+	
+	private String name;
+    private String nombre;
+
+	Country(String name, String nombre) {
+        this.name = name;
+        this.nombre = nombre;
+	}
+
+	public boolean contains(String value) {
+		return this.toString().contains(value.toUpperCase());
+	}
+	
+	// getters
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+	public static List<Country> getAll() {
+		return Arrays.asList(values());
+	}
+}
